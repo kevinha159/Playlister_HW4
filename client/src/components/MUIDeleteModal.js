96a68e0d8963b9683f3224061868142a83a6cf19
Kevin Ha <kevinha159@gmail.com>
2022-11-07 02:06:52 -0500

    List Deletion

--- client/src/components/MUIDeleteModal.js
+++ client/src/components/MUIDeleteModal.js
@@ -10,10 +10,10 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
-    bgcolor: 'background.paper',
+    bgcolor: 'background.black',
     border: '2px solid #000',
     boxShadow: 24,
-    p: 4,
+    borderRadius: 2
 };
 
 export default function MUIDeleteModal() {
