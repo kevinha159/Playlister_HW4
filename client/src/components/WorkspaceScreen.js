9d013535a7e96fd2add8c6acbe21f5fe896ac82c
Kevin Ha <kevinha159@gmail.com>
2022-11-07 02:03:34 -0500

    User Loading, Editing and Deleting Owned Lists

--- client/src/components/WorkspaceScreen.js
+++ client/src/components/WorkspaceScreen.js
@@ -6,6 +6,7 @@ import MUIRemoveSongModal from './MUIRemoveSongModal'
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import { GlobalStoreContext } from '../store/index.js'
+import AuthContext from '../auth/index.js'
 /*
     This React component lets us edit a loaded list, which only
     happens when we are on the proper route.
@@ -15,6 +16,8 @@ import { GlobalStoreContext } from '../store/index.js'
 function WorkspaceScreen() {
     const { store } = useContext(GlobalStoreContext);
     store.history = useHistory();
+    const { auth } = useContext(AuthContext);
+
     
     let modalJSX = "";
     if (store.isEditSongModalOpen()) {
@@ -22,6 +25,29 @@ function WorkspaceScreen() {
     }
     else if (store.isRemoveSongModalOpen()) {
         modalJSX = <MUIRemoveSongModal />;
+    }
+
+    if(store.currentList){
+        console.log(store.currentList);
+        if(auth.user){
+            if(!store.currentList.ownerEmail || !auth.user.email){
+                store.history.push('/');
+                store.currentList = null;
+                return <div></div>;
+            }
+            if(store.currentList.ownerEmail !== auth.user.email){
+                store.history.push('/');
+                store.currentList = null;
+                return <div></div>;
+            }
+        }else{
+            store.history.push('/');
+            store.currentList = null;
+            return <div></div>
+        }
+    }else{
+        store.history.push('/');
+        return <div></div>
     }
     return (
         <Box>
